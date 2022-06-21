5545def1169e6b52cdc14ca4e6abfe0993cb8e27
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-06-21 10:45:29 +0100

    Challenge on React Properties Apply CSS styles to match the appearance on the completed app. Extract the contact card as a reusable Card component.Use props to render the default Beyonce contact card so the Card component can be reused for other contacts. Import the contacts.js file to create card components.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,30 @@
 import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2 className="name">Beyonce</h2>
-          <img
-            className="circle-img"
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p className="info">+123 456 789</p>
-          <p className="info">b@beyonce.com</p>
-        </div>
-      </div>
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
     </div>
   );
 }
-
 export default App;
